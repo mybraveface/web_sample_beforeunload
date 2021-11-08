2bba5f97d89a84043ec2d2346cec83ec8dda1814
Kojiro Shoji <shoji@sria.co.jp>
2021-11-08 13:08:19 +0900

    ios safari 対応

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 document.getElementById('btnRegister').addEventListener('click', () => {
-    window.onbeforeunload = null;
-    window.onbeforeunload = function(e) {
-        return 'このページを離れますか？';
+    window.onpagehide = null;
+    window.onpagehide = function(e) {
+        saveLocalStorage()
     }
     console.log('Registered');
 });
 document.getElementById('btnUnregister').addEventListener('click', () => {
-    window.onbeforeunload = null;
+    window.onpagehide = null;
     console.log('Unregistered');
 });
-// window.onbeforeunload = function(e){
-//     return "このページを離れますか？"; // Google Chrome以外
-//     e.returnValue = "このページを離れますか？"; // Google Chrome
-// }
+document.getElementById('btnClearLocalStorage').addEventListener('click', () => {
+    localStorage.removeItem('input');
+    console.log('cleared');
+});
+
+function saveLocalStorage() {
+    const value = {
+        'value1' : document.getElementById('value1').value,
+        'value2' : document.getElementById('value2').value,
+        'value3' : document.getElementById('value3').value
+    };
+    localStorage.setItem('input', JSON.stringify(value));
+}
+
+window.onload = () => {
+    const value = localStorage.getItem('input');
+    if (value == null) {
+        return;
+    }
+
+    const valueObj = JSON.parse(value);
+
+    document.getElementById('value1').value = valueObj.value1;
+    document.getElementById('value2').value = valueObj.value2;
+    document.getElementById('value3').value = valueObj.value3;
+}
